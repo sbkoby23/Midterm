9dbbe32f69e4038e593a73785f0880bec0b4a94b
sbkoby23 <99813287+sbkoby23@users.noreply.github.com>
2022-02-25 14:35:58 -0500

    Update a.py

--- a.py
+++ a.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def main(data, y):
+    #Given data and y column, outputs mean and mean square of each column
     averages = np.mean(data,axis=0)
     mean_square = np.mean(data**2, axis=0)
     return averages, mean_square
