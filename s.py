d135ee675f4cac4b86f81ab0766b52ba572289d7
sbkoby23 <99813287+sbkoby23@users.noreply.github.com>
2022-02-25 14:36:35 -0500

    Update s.py

--- s.py
+++ s.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 def main(data, y):
+    #Given data and the y column, computes ratio of mean of y column divided by x columns
     averages = np.mean(data, axis=0)
     return averages[y-1]/np.concatenate((averages[0:y-1], averages[y:len(averages)]))
